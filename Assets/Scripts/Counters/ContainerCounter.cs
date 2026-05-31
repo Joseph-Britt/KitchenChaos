91eea030e8704e62e62698e8fa2e2c1c2c1a0244
Joseph Britt <48034902+Joseph-Britt@users.noreply.github.com>
2026-05-31 12:48:52 -0400

    ContainerCounter QoL Improvements
    Updated the container counters to allow ingredients to be picked up directly onto a plate instead of requiring empty hands to pick up.

--- Assets/Scripts/Counters/ContainerCounter.cs
+++ Assets/Scripts/Counters/ContainerCounter.cs
@@ -2,6 +2,8 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using static StoveCounter;
+using static UnityEngine.CullingGroup;
 
 public class ContainerCounter : BaseCounter {
 
@@ -13,7 +15,13 @@ public class ContainerCounter : BaseCounter {
 
 
     public override void Interact(Player player) {
-        if (!player.HasKitchenObject()) {
+        if (player.HasKitchenObject()) {
+            // Player is carrying something
+            if (player.GetKitchenObject().TryGetPlate(out PlateKitchenObject plateKitchenObject)) {
+                // Player is holding a Plate
+                plateKitchenObject.TryAddIngredient(kitchenObjectSO);
+            }
+        } else {
             // Player is not carrying anything 
             KitchenObject.SpawnKitchenObject(kitchenObjectSO, player);
 
